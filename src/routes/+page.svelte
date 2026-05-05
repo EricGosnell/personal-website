56a87759bc003330888a9c9a5afb5ae3a205fa14
Eric Gosnell <eric.gosnell@colorado.edu>
2026-05-04 19:24:26 -0600

    Update homepage to test github actions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Eric Gosnell's Portfolio</h1>
+
+<p>Coming soon...</p>
